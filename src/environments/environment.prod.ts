84dce8ba74831f262d31d8ccb466a0ed74af5ca9
Ruslan Gonzalez <ruslanguns@gmail.com>
2021-12-02 20:15:00 +0000

    ng add @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'ruslanbot-66a53',
+    appId: '1:668084533205:web:2ddc5088d4780bf5c56f5c',
+    databaseURL: 'https://ruslanbot-66a53.firebaseio.com',
+    storageBucket: 'ruslanbot-66a53.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAsUxa7R0aNWEJlA-VD6MPZajZMMW4M8W0',
+    authDomain: 'ruslanbot-66a53.firebaseapp.com',
+    messagingSenderId: '668084533205',
+  },
   production: true
 };
