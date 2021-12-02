d75711294a9d89c5cc67e54ad2ca2d449aad5029
Ruslan Gonzalez <ruslanguns@gmail.com>
2021-12-02 21:36:03 +0000

    Configuración de formulario reactivo

--- src/app/task/task.page.ts
+++ src/app/task/task.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.page.scss'],
 })
 export class TaskPage implements OnInit {
+  ngForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.ngForm = this.fb.group({
+      description: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(500),
+        ],
+      ],
+    });
   }
 
+  get description() {
+    return this.ngForm.get('description');
+  }
+
+  get descriptionLength() {
+    return this.description?.value?.length;
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.ngForm.valid) {
+      console.log(this.ngForm.value);
+    } else {
+      console.log('El formulario no es valido');
+    }
+  }
 }
